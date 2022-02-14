fa2c7000b2df8623465c8b4924145d72d3d31a64
nileshgulia1 <nileshgulia@gmail.com>
2022-02-14 18:08:27 +0530

    remove comma after tests

--- eea/dexterity/indicators/behaviors/indicator.py
+++ eea/dexterity/indicators/behaviors/indicator.py
@@ -53,7 +53,7 @@ def dedupe_data(data):
     >>> result = dedupe_data(value)
     >>> next(result)
     {'type': 'p',
-    'children': [{'url': 'https://www.eea.europa.eu', 'text': ''}]},
+    'children': [{'url': 'https://www.eea.europa.eu', 'text': ''}]}
 
     """
     existing = set()
