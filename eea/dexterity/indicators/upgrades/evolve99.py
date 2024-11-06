199aa7b76531e3e09e629713c79949824943d978
alin <contact@avoinea.com>
2024-11-06 20:50:03 +0200

    Use ZLogHandler to report progress

--- eea/dexterity/indicators/upgrades/evolve99.py
+++ eea/dexterity/indicators/upgrades/evolve99.py
@@ -1,21 +1,17 @@
 
 """ Upgrade to 9.9 """
-import logging
 from Products.CMFCore.utils import getToolByName
-logger = logging.getLogger("eea.dexterity.indicators")
+from Products.ZCatalog.ProgressHandler import ZLogHandler
 
 
 def to_99(context):
     """ Upgrade to 9.9 """
-    logger.warning("Reindex IMS indicator SearchableText...")
     ctool = getToolByName(context, "portal_catalog")
-    portal_type = "ims_indicator"
-    brains = ctool.unrestrictedSearchResults(portal_type=portal_type)
-    idx, total = 0, len(brains)
+    brains = ctool.unrestrictedSearchResults(portal_type="ims_indicator")
+    pghandler = ZLogHandler(100)
+    pghandler.init("Reindex IMS indicator SearchableText", len(brains))
     for idx, brain in enumerate(brains):
+        pghandler.report(idx)
         indicator = brain.getObject()
         indicator.reindexObject(idxs=['SearchableText'])
-        if idx % 100 == 0:
-            logger.warning(
-                "Reindex IMS indicator SearchableText...%s/%s", idx, total)
-    logger.warning("Reindex IMS indicator SearchableText... %s/%s", idx, total)
+    pghandler.finish()
